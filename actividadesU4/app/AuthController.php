68419c68bcee6f8f4898f2a8888fed33e76af127
AlbertoCarballo <161271994+AlbertoCarballo@users.noreply.github.com>
2024-10-22 20:05:53 -0700

    Actividad 15 Unidad 4
    Carballo Caballero Jesus Alberto

---
+++ actividadesU4/app/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+    // var_dump($_POST);
+
+    if (isset($_POST["action"]) && $_POST["action"] === 'access') {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $controller = new Controller;
+
+        $controller->access($email, $password);
+    }
+
+    class Controller {
+
+        public function access($email, $password) {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://crud.jonathansoto.mx/api/login',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => array(
+                    'email' => $email,     
+                    'password' => $password 
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $response=json_decode($response);
+
+
+            if (isset($response->message) && $response->message === "Registro obtenido correctamente" 
+            && is_object($response->data)) {
+                
+                header("Location: ../home.html");
+            }else{
+                header("Location: ../index.html");
+            }
+        }
+    }
+?>
